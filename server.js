3186358214304def1008fd81fbf27684be5816d5
vahidR <vahid.rafiei@gmail.com>
2023-04-13 19:48:28 +0200

    Importing productRouter and some cleanups.

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const productRouter = require('./routes/productRouter');
+
 const PORT = process.env.PORT || 8080;
-
-const app = express();
-
 var corsOptions = {
     origin: "http://localhost:8081"
 };
 
 // Middleware
+const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api/v1/products', productRouter);
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to my application.' });
