df5679d0da7876d126b722fd95b621b019ea95d2
vahidR <vahid.rafiei@gmail.com>
2023-04-13 17:46:30 +0200

    For some reasons, model/index.js was not added!

---
+++ models/index.js
@@ -0,0 +1,38 @@
+const dbConfig = require('../config/dbConfig.js');
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize(
+    dbConfig.DB,
+    dbConfig.USER,
+    dbConfig.PASSWORD, {
+        host: dbConfig.HOST,
+        dialect: dbConfig.dialect,
+        operatorsAliases: false,
+
+        pool: {
+            max: dbConfig.pool.max,
+            min: dbConfig.pool.min,
+            acquire: dbConfig.pool.acquire,
+            idle: dbConfig.pool.idle
+        }
+    }
+);
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+}).catch(err => {
+    console.error('Unable to connect to the database:', err);
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.products = require('./productModel.js')(sequelize, DataTypes);
+db.reviews = require('./reviewModel.js')(sequelize, DataTypes);
+
+// froce: false is important as it PREVENTS to earase the data every time !!
+db.sequelize.sync({ force: false }).then(() => {
+    console.log('Yes, re-sync done...');
+});
